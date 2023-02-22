519449107b01e402268f0d4d95f378702774abc6
Ajay Chaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-22 20:54:20 +0530

    my_weather_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Header from "./components/Header";
+import "./App.css";
+import WeatherCard from "./components/WeatherCard"
+import axios from 'axios';
+
+const URL = `https://api.openweathermap.org/data/2.5/weather`
+const API_KEY = `ffbddb778df8adb6ac415b47d213ed91`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [latitude, setLatitude] = useState(17.41);
+  const [longitude, setLongitude] = useState(78.26);
+  const [city, setCity] = useState('Hyderabad');
+  const [temprature, setTemprature] = useState('');
+  const [humidity, setHumidity] = useState(null);
+  const [sunrise, setSunrise] = useState(null);
+  const [sunset, setSunset] = useState(null);
 
+  useEffect(() => {
+  navigator.geolocation.getCurrentPosition(function(position){
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+    });
+
+    axios.get(`${URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    .then((weatherData)=>{
+      console.log(weatherData.data);
+      setTemprature(weatherData.data.main.temp)
+      setSunset(weatherData.data.sys.sunset)
+      setSunrise(weatherData.data.sys.sunrise)
+      setHumidity(weatherData.data.main.humidity)
+      setCity(weatherData.data.name)
+    })
+  }, [latitude, longitude])
+  
+  return (
+    <div className="main">
+     <Header /> 
+     <WeatherCard 
+     temprature={temprature}
+     humidity={humidity}
+     sunrise={sunrise}
+     sunset={sunset}
+     city={city}
+     />
+    </div>
+  )
+}
 export default App;
